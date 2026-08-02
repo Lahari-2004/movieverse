51b8f2ecfde0093556e81fa35d01d4f7b5157234
Lahari Reddy <laharireddy@Laharis-MacBook-Air.local>
2026-08-02 22:48:32 +0530

    added customuserdetailsservice

--- backend/src/main/java/com/movieverse/backend/security/JwtService.java
+++ backend/src/main/java/com/movieverse/backend/security/JwtService.java
@@ -1,26 +1,36 @@
 package com.movieverse.backend.security;
 
-import org.springframework.stereotype.Service;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
     @Value("${jwt.secret}")
     private String secretKey;
 
     @Value("${jwt.expiration}")
     private long jwtExpiration;
 
+    /**
+     * Generate the signing key
+     */
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
+    /**
+     * Generate JWT Token
+     */
     public String generateToken(String email) {
 
         return Jwts.builder()
@@ -30,4 +40,57 @@ public class JwtService {
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    /**
+     * Extract Username (Email)
+     */
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    /**
+     * Extract Expiration Date
+     */
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    /**
+     * Generic Method to Extract Claims
+     */
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    /**
+     * Extract All Claims from JWT
+     */
+    private Claims extractAllClaims(String token) {
+
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    /**
+     * Check if Token is Expired
+     */
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    /**
+     * Validate JWT Token
+     */
+    public boolean isTokenValid(String token, String email) {
+
+        final String username = extractUsername(token);
+
+        return username.equals(email)
+                && !isTokenExpired(token);
+    }
+
 }
